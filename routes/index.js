55220ef7dbff17508d3fe705c225d6569338f8fa
Godwin Idemudia <godwinide@gmail.com>
2023-06-17 05:32:55 +0100

    new

--- routes/index.js
+++ routes/index.js
@@ -43,13 +43,14 @@ router.post("/webhook", async (req, res) => { //i want some
 
 
 const model = new OpenAI({ openAIApiKey: process.env.openAIApiKey, temperature: 0.9 });
-const memory = new BufferMemory();
-const chain = new ConversationChain({ llm: model, memory });
-const systemMessage = new SystemChatMessage("You are an AI girlfriend that responds romantically to any question");
-chain.call({ input: systemMessage });
+
 
 const airesponse = async (res, msg, phon_no_id, from) => {
+    const memory = new BufferMemory();
+    const chain = new ConversationChain({ llm: model, memory });
 
+    const systemMessage = new SystemChatMessage("You are an AI girlfriend that responds romantically to any question");
+    await chain.call({ input: systemMessage });
 
     const data = await chain.call({ input: msg });
 
